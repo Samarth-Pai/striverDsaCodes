19695cadb65ae0bec4fc8a014c1ba3a73f6e071f
Samarth Pai <samarthpai9870@gmail.com>
2024-09-28 20:26:23 +0530

    Added the optimal solution for arrays/3sum.cpp

--- arrays/3sum.cpp
+++ arrays/3sum.cpp
@@ -28,23 +28,54 @@ using namespace std;
 // Better approach: hashing
 // TC: O(N^2*logN)
 // SC: O(N) + O(no. of unique triplets) * 2
+// vector<vector<int>> threeSum(vector<int> nums){
+//     set<vector<int>> st;
+//     int n = nums.size();
+//     for(int i = 0;i<n;i++){
+//         set<int>hashset;
+//         for(int j = i+1;j<n;j++){
+//             int third = -(nums[i] + nums[j]);
+//             if(hashset.find(third)!=hashset.end()){
+//                 vector<int>temp = {nums[i], nums[j], third};
+//                 sort(temp.begin(), temp.end());
+//                 st.insert(temp);
+//             }
+//             hashset.insert(nums[j]);
+//         }
+//     }
+//     vector<vector<int>> res(st.begin(), st.end());
+//     return res;
+// }
+
+// Optimal solution: 2 pointer approch
+// TC: O(NlogN) + O(N^2)
+// SC: O(no. of unique triplets)
 vector<vector<int>> threeSum(vector<int> nums){
-    set<vector<int>> st;
-    int n = nums.size();
+    int n = nums.size(), j, k;
+    vector<vector<int>> ans;
+    sort(nums.begin(), nums.end());
     for(int i = 0;i<n;i++){
-        set<int>hashset;
-        for(int j = i+1;j<n;j++){
-            int third = -(nums[i] + nums[j]);
-            if(hashset.find(third)!=hashset.end()){
-                vector<int>temp = {nums[i], nums[j], third};
-                sort(temp.begin(), temp.end());
-                st.insert(temp);
+        if(i>0 and nums[i] == nums[i-1]) continue;
+        j = i+1;
+        k = n-1;    
+        while(j<k){
+            int sum = nums[i] + nums[j] + nums[k];
+            if(sum<0){
+                j++;
             }
-            hashset.insert(nums[j]);
+            else if(sum>0){
+                k--;
+            }
+            else{
+                ans.push_back({nums[i], nums[j], nums[k]});
+                j++;
+                k--;
+                while(j<k and nums[j]==nums[j-1]) j++;
+                while(j<k and nums[k]==nums[k+1]) k--;
+            }
         }
     }
-    vector<vector<int>> res(st.begin(), st.end());
-    return res;
+    return ans;
 }
 
 int main(){
