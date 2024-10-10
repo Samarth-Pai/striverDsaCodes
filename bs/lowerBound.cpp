3c8e20ac95be25477a2f18bf36f1d3190c655fd7
Samarth Pai <samarthpai9870@gmail.com>
2024-10-10 15:34:39 +0530

    Added manual solution to bs/lowerBound.cpp

---
+++ bs/lowerBound.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int lowerBound(vector<int> arr, int n, int x) {
+    int ans = n, low = 0, high = n-1, mid;
+    while(low<=high){
+        mid = (low + high)/2;
+        if(arr[mid]>=x){
+            ans = mid;
+            high = mid - 1;
+        }
+        else low = mid + 1;
+    }
+    return ans;
+}
+int main(){
+    int n, ele, target;
+    vector<int> arr;
+    cout<<"Enter the array size: ";
+    cin>>n;
+    cout<<"Enter the array elements: ";
+    for(int i = 0;i<n;i++){
+        cin>>ele;
+        arr.push_back(ele);
+    }
+    cout<<"Enter the target: ";
+    cin>>target;
+    cout<<"Lower bound of the array is "<<lowerBound(arr, n, target)<<endl;
+}
