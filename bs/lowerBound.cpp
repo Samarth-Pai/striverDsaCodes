56b3535713df49ad6d1bb93992d17135d586ab7a
Samarth-Pai <samarthpai9870@gmail.com>
2024-10-11 19:43:55 +0530

    Added stl solution for bs/lowerBound.cpp

--- bs/lowerBound.cpp
+++ bs/lowerBound.cpp
@@ -1,18 +1,26 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int lowerBound(vector<int> arr, int n, int x) {
-    int ans = n, low = 0, high = n-1, mid;
-    while(low<=high){
-        mid = (low + high)/2;
-        if(arr[mid]>=x){
-            ans = mid;
-            high = mid - 1;
-        }
-        else low = mid + 1;
-    }
-    return ans;
+
+// TC: O(log2 N)
+// int lowerBound(vector<int> arr, int n, int x) {
+//     int ans = n, low = 0, high = n-1, mid;
+//     while(low<=high){
+//         mid = (low + high)/2;
+//         if(arr[mid]>=x){
+//             ans = mid;
+//             high = mid - 1;
+//         }
+//         else low = mid + 1;
+//     }
+//     return ans;
+// }
+
+// STL
+int lowerBound(vector<int> arr, int n, int x){
+    return lower_bound(arr.begin(), arr.end(), x) - arr.begin();
 }
+
 int main(){
     int n, ele, target;
     vector<int> arr;
